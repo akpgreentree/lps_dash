9eac4e3b3cb8695338e4d5f69775fe0e6547e5b9
akpgreentree <agtree@uchicago.edu>
2020-09-23 11:34:32 -0700

    adjust for running locally

--- app.py
+++ app.py
@@ -1,23 +1,16 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
-import debug_component
 import components
 
 app = dash.Dash(__name__)
 
-timecourse = components.TopicTimecourse('/project2/nchevrier/projects/cytokines/output/tables/bulkRNAseq/LPS/topics.csv')
-cpm_plotter = components.CPMPlotter('/project2/nchevrier/projects/cytokines/output/tables/bulkRNAseq/LPS/cpm.gct')
-z_table = components.GeneTable('/project2/nchevrier/projects/cytokines/output/tables/bulkRNAseq/LPS/topic_Z.csv',
+timecourse = components.TopicTimecourse('data/topics.csv')
+cpm_plotter = components.CPMPlotter('data/cpm.gct')
+z_table = components.GeneTable('data/topic_Z.csv',
                                cpm_plotter, app, 'Z')
-lfc_table = components.GeneTable('/project2/nchevrier/projects/cytokines/output/tables/bulkRNAseq/LPS/topic_lfc.csv',
+lfc_table = components.GeneTable('data/topic_lfc.csv',
                                  cpm_plotter, app, 'beta')
-
-
-input_debugger = debug_component.DebuggerComponent(app, 
-                                                   'topic-timecourse', 
-                                                   'clickData')
-
 
 app.layout = html.Div(children = [
     html.Div(
@@ -34,17 +27,11 @@ app.layout = html.Div(children = [
                 lfc_table.get_component()
             ])
         ]
-    ),
-    input_debugger.get_component()
+    )
 ])
 
-input_debugger.register()
 z_table.register()
 lfc_table.register()
 
 if __name__ == '__main__':
-    from subprocess import run
-    from random import randrange
-    host = run(['hostname', '-i'], capture_output= True).stdout
-    port = randrange(8000, 9000)
-    app.run_server(debug=True, port=port, host=host[:-1].decode("utf-8"))
+    app.run_server(debug=True)
